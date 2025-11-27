b839cf0e04b7e700c046ee7d3ea08392dbfe27cc
inkwon <dlsrnjs125@naver.com>
2025-11-27 22:50:57 +0900

    feat: setup API v1 routing structure

---
+++ api/v1/urls.py
@@ -0,0 +1,13 @@
+"""
+URL configuration for API v1
+"""
+
+from django.urls import include, path
+
+
+app_name = "api_v1"
+
+urlpatterns = [
+    # TODO: 대출 도메인 API 엔드포인트 구현 예정
+    # path("", include("api.v1.loans.urls")),
+]
